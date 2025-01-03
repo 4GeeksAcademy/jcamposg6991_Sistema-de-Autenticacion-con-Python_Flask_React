29b797b051815668dd7d5995d4d6c59d949df76c
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-03 22:33:18 +0000

    adding autoincrement property to id in user table

--- src/api/models.py
+++ src/api/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement= True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     name = db.Column(db.String(80), unique=False, nullable=False)
